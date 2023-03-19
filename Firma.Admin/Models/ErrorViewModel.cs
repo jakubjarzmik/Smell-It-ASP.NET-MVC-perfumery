d08c968feef11c84f98c2dc1b628887194b68f20
Jakub Jarzmik <jakubjarzmik00@gmail.com>
2023-03-19 20:01:23 +0100

    Add new layout and edit privacy

--- Firma.Admin/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Firma.Admin.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
